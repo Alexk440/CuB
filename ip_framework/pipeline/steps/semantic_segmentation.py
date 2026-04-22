639210a9a67a36e06adc87dd6cad36d8ffa6b9b9
Ruby11235 <152699064+Ruby11235@users.noreply.github.com>
2026-04-22 14:29:28 +0200

    image_mask implemented

--- ip_framework/pipeline/steps/semantic_segmentation.py
+++ ip_framework/pipeline/steps/semantic_segmentation.py
@@ -16,6 +16,8 @@ class SemanticSegmentationStep(Step):
         self.models = {}
 
     def load_model(self, model_name):
+        if self.models.get(model_name) is not None:
+            return self.models[model_name]
         if model_name == 'fcn':
             model = fcn_resnet101(weights=FCN_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1)
         elif model_name == 'deeplab':
@@ -29,34 +31,57 @@ class SemanticSegmentationStep(Step):
 
     def apply(self, input_img: np.ndarray, config: dict = None) -> StepResult:
 
-        model_name = config.get('model', 'fcn').lower()
+        model_name = config.get('model')
 
-        model = self.models.get(model_name)
-        if model is None:
-            model = self.load_model(model_name)
+        model = self.load_model(model_name)
+
+        if input_img.shape[-1] == 4:
+            input_img = input_img[..., :3]
 
         rgb_img = np.array(input_img, dtype=np.uint8, copy=True)
 
-        input_tensor = transforms.ToTensor()(rgb_img)
-
-        # Perform the segmentation.
-        with torch.no_grad():
-            output = model(input_tensor)['out'][0]
-
-        # Convert output to a segmentation mask.
-        seg_mask = output.argmax(0).byte().cpu().numpy()
+        input_tensor = transforms.ToTensor()(rgb_img).unsqueeze(0)
 
         # Map the segmentation mask to colors.
-        seg_mask_color = self.visualize_segmentation_mask(seg_mask)
+        seg_mask_color = self.visualize_segmentation_mask(model(input_tensor))
 
         # Return the color segmentation mask as the output.
         return StepResult(seg_mask_color)
 
     def visualize_segmentation_mask(self, seg_mask: np.ndarray):
+        colors = [
+            [0, 0, 0],
+            [128, 0, 0],
+            [0, 128, 0],
+            [128, 128, 0],
+            [0, 0, 128],
+            [128, 0, 128],
+            [0, 128, 128],
+            [128, 128, 128],
+            [64, 0, 0],
+            [192, 0, 0],
+            [64, 128, 0],
+            [192, 128, 0],
+            [64, 0, 128],
+            [192, 0, 128],
+            [64, 128, 128],
+            [192, 128, 128],
+            [0, 64, 0],
+            [128, 64, 0],
+            [0, 192, 0],
+            [128, 192, 0],
+            [0, 64, 128],
+        ]
 
-        # TODO Create color_mask based on seg_mask. Use COCO/VOC-like classes with basic colors.
+        predictions = seg_mask['out'][0]
+        seg_mask_indices = torch.argmax(predictions, dim=0)
 
-        color_mask = None
+        h, w = seg_mask_indices.shape
+        color_mask = np.zeros((h, w, 3))
+
+        for class_idx, color in enumerate(colors):
+            color_mask[seg_mask_indices == class_idx] = color
+
         return color_mask
 
     def config_schema(self):
